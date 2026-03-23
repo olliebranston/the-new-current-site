cfebd741cf115ecec5d01012ac6209942b4c050d
Ollie Branston <ollie.branston123@gmail.com>
2026-03-23 10:05:43 +0000

    Add first carbon intensity chart to data page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,58 @@
 console.log("The New Current loaded successfully");
+
+const chartCanvas = document.getElementById("carbonChart");
+
+if (chartCanvas) {
+  const labels = [
+    "00:00",
+    "01:00",
+    "02:00",
+    "03:00",
+    "04:00",
+    "05:00",
+    "06:00",
+    "07:00",
+    "08:00",
+    "09:00"
+  ];
+
+  const carbonIntensityValues = [128, 121, 119, 115, 117, 125, 138, 149, 156, 152];
+
+  new Chart(chartCanvas, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Carbon intensity",
+          data: carbonIntensityValues,
+          borderWidth: 2,
+          tension: 0.2
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: true
+        }
+      },
+      scales: {
+        y: {
+          beginAtZero: false,
+          title: {
+            display: true,
+            text: "gCO2/kWh"
+          }
+        },
+        x: {
+          title: {
+            display: true,
+            text: "Time"
+          }
+        }
+      }
+    }
+  });
+}
